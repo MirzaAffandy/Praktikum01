57ee3345403d2f27c2b8daa3bd2450b73faa7b4e
MirzaAffandy <mirza.afandi@ymail.com>
2014-01-04 11:16:32 +0700

    commit21

---
+++ pemrograman 01/praktikum1/LatihanA.java
@@ -0,0 +1,14 @@
+//D:\Pemprograman 1\GIT\sesi 01>
+//javac -d bin Praktikum1/LatihanA.java
+
+//java -cp bin
+//Latihan Anatomi Aplikasi Java
+//Classpath
+
+package Latihan01.sesi2.bin;
+
+public class LatihanA
+
+{
+
+}
---
+++ pemrograman 01/praktikum1/LatihanE.java
@@ -0,0 +1,31 @@
+//D:\Pemprograman 1\GIT\sesi01>
+//javac -d bin Praktikum1/LatihanE.java
+
+//java -cp bin
+//Latihan Anatomi Aplikasi Java
+//Classpath
+
+package Latihan02.Praktikum01.bin;
+
+public class LatihanE
+
+{
+     private Long id;
+	 private String nama;
+	 private String getNama()
+	 {
+	     return nama;
+	}
+	public void setNama(String nm)
+	{
+	     nama = nm;
+	}
+	public Long getId()
+	{
+	     return id;
+    }
+	public void setId(long i)
+	{
+	     id = i;
+	 }
+}
