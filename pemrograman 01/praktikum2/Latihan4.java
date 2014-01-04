8270f2c6b75f669aa7e8097921d7637247ff9d35
MirzaAffandy <mirza.afandi@ymail.com>
2014-01-04 11:31:09 +0700

    commit22

---
+++ pemrograman 01/praktikum2/Latihan4.java
@@ -0,0 +1,14 @@
+package Praktikum01.bin;
+
+public class Latihan4
+{
+   public static void main(String[] args) 
+   {
+      int value = 10;
+	  char a;
+	  a = 'B';
+	  
+	  System.out.println(value);
+	  System.out.println("The value of \"a\" = " +a);
+	  }
+	}
